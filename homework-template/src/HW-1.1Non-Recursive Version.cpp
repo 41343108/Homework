3c0cce3c48e9cfcb2109de2cbac32f01ed3544c3
41343108 <41343108@nfu.edu.tw>
2025-10-13 19:57:46 +0800

    Update HW-1.1Non-Recursive Version.cpp

--- homework-template/src/HW-1.1Non-Recursive Version.cpp
+++ homework-template/src/HW-1.1Non-Recursive Version.cpp
@@ -1,15 +1,38 @@
 #include <iostream>
+#include <stack>
 using namespace std;
 
-int sigma(int n) {
-    if (n < 0)
-        throw "n < 0";
-    else if (n <= 1)
-        return n;
-    return n + sigma(n - 1);
+unsigned long long AckermannNonRecursive(unsigned int m, unsigned int n) {
+    stack<unsigned int> s;
+    s.push(m);
+
+    while (!s.empty()) {
+        m = s.top();
+        s.pop();
+
+        if (m == 0) {
+            n = n + 1;
+        } 
+        else if (n == 0) {
+            s.push(m - 1);
+            n = 1;
+        } 
+        else {
+            s.push(m - 1);
+            s.push(m);
+            n = n - 1;
+        }
+    }
+    return n;
 }
 
 int main() {
-    int result = sigma(3);
-    cout << result << '\n';
+    unsigned int m, n;
+
+    cout << "請輸入 m 和 n: ";
+    cin >> m >> n;
+
+    cout << "Ackermann(" << m << ", " << n << ") = " << AckermannNonRecursive(m, n) << endl;
+
+    return 0;
 }
