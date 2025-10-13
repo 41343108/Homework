f338cb0b2b9680dba48dd2b38fbd0c756869cc80
41343108 <41343108@nfu.edu.tw>
2025-10-13 22:23:47 +0800

    Update HW-1.2.cpp

--- homework-template/src/HW-1.2.cpp
+++ homework-template/src/HW-1.2.cpp
@@ -1,42 +1,30 @@
 #include <iostream>
 #include <vector>
-#include <string>
 using namespace std;
 
-void generatePowerset(vector<string>& set, int index, vector<string> current, vector<vector<string>>& result) {
+void powerset(vector<char>& set, vector<char>& subset, int index) {
     if (index == set.size()) {
-        result.push_back(current);
+        
+        cout << "{ ";
+        for (char c : subset) {
+            cout << c << " ";
+        }
+        cout << "}" << endl;
         return;
     }
 
-    generatePowerset(set, index + 1, current, result);
+    powerset(set, subset, index + 1);
 
-    current.push_back(set[index]);
-    generatePowerset(set, index + 1, current, result);
+    subset.push_back(set[index]);
+    powerset(set, subset, index + 1);
+
+    subset.pop_back();
 }
 
 int main() {
-    int n;
-    cout << "請輸入集合元素個數 n: ";
-    cin >> n;
-
-    vector<string> set(n);
-    cout << "請輸入集合元素（以空白分隔）: ";
-    for (int i = 0; i < n; i++) cin >> set[i];
-
-    vector<vector<string>> result;
-    vector<string> current;
-
-    generatePowerset(set, 0, current, result);
-
-    cout << "\nPowerset(S) = {" << endl;
-    for (auto subset : result) {
-        cout << "  { ";
-        for (auto elem : subset) cout << elem << " ";
-        cout << "}" << endl;
-    }
-    cout << "}" << endl;
-
+    vector<char> S = {'a', 'b', 'c'};
+    vector<char> subset;
+    cout << "Powerset:" << endl;
+    powerset(S, subset, 0);
     return 0;
 }
-
