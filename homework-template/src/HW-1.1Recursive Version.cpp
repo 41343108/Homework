5a89c8bda61e100fa2962a55be52f39c1232cc4c
41343108 <41343108@nfu.edu.tw>
2025-10-13 20:00:49 +0800

    Update HW-1.1Recursive Version.cpp

--- homework-template/src/HW-1.1Recursive Version.cpp
+++ homework-template/src/HW-1.1Recursive Version.cpp
@@ -3,16 +3,12 @@ using namespace std;
 
 unsigned long long Ackermann(unsigned int m, unsigned int n) {
     if (m == 0) {
-        // 當 m = 0 時，回傳 n + 1
         return n + 1;
     } 
     else if (n == 0) {
-        // 當 n = 0 時，呼叫 A(m - 1, 1)
         return Ackermann(m - 1, 1);
     } 
     else {
-        // 一般情況，呼叫 A(m - 1, A(m, n - 1))
-        // 先計算內層 A(m, n - 1)，再把結果代入 A(m - 1, ...)
         return Ackermann(m - 1, Ackermann(m, n - 1));
     }
 }
@@ -23,7 +19,6 @@ int main() {
     cout << "請輸入 m 和 n: ";
     cin >> m >> n;
 
-    // 呼叫遞迴版本 Ackermann 函數
     cout << "Ackermann(" << m << ", " << n << ") = " << Ackermann(m, n) << endl;
 
     return 0;
